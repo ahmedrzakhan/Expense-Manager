00142de80b150cb44a7104e433d60a0571205f81
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-06-18 18:16:27 +0530

    Input Container

--- ExpenseManager.js
+++ ExpenseManager.js
@@ -1,3 +1,20 @@
 window.addEventListener("load", function () {
-  console.log("a");
+  var form = document.getElementById("form");
+  form.addEventListener("submit", onFormSubmit);
+
 });
+
+var onFormSubmit = function() {
+  event.preventDefault();
+  var elements = event.target.children;
+  console.log("elements", elements);
+
+  var type = elements[0].value;
+  console.log("elements", type);
+
+  var source = elements[2].value;
+  console.log("source", source);
+
+  var amount = elements[4].value;
+  console.log("amount", amount);
+}
