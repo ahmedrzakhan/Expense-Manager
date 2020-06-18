99c1c0885fe9f8d62646aef1f03a7adb465c0cf7
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-06-18 21:09:05 +0530

    Render Table Functionality

--- ExpenseManager.js
+++ ExpenseManager.js
@@ -1,8 +1,26 @@
+// Global Object
+var manager = {
+  records: [],
+  totalExpenses: 0,
+  totalIncomes: 0,
+  currentBalance: 0,
+
+  // Add Transaction
+  addTransaction: function (item) {
+    // Push item in Global Array
+    this.records.push(item);
+    // Render table
+    renderTable(this.records);
+    console.log("records", this.records);
+  },
+};
+
 window.addEventListener("load", function () {
   var form = document.getElementById("form");
   form.addEventListener("submit", onFormSubmit);
 });
 
+// On Form  Submit
 var onFormSubmit = function () {
   event.preventDefault();
   var elements = event.target.children;
@@ -16,4 +34,57 @@ var onFormSubmit = function () {
 
   var amount = elements[4].value;
   console.log("amount", amount);
+
+  var data = {
+    id: Number(manager.records.length) + 1,
+    type: type,
+    source: source,
+    amount: amount,
+  };
+
+  manager.addTransaction(data);
+};
+
+// Render Table
+var renderTable = function (item) {
+  var recordsTable = document.getElementById("recordsTable");
+  // Empty Table
+  recordsTable.innerHTML = "";
+
+  // First Row
+  var firstRow = createRow({
+    id: "Id",
+    type: "Type",
+    source: "Source",
+    amount: "Amount",
+  });
+
+  recordsTable.append(firstRow);
+
+  for (var i = 0; i < item.length; i++) {
+    // Create Row
+    var currentRow = createRow(item[i]);
+
+    recordsTable.append(currentRow);
+  }
+};
+
+var createRow = function (elem) {
+  var row = document.createElement("tr");
+
+  var idColumn = document.createElement("td");
+  idColumn.textContent = elem.id;
+
+  var typeColumn = document.createElement("td");
+  typeColumn.textContent = elem.type;
+
+  var sourceColumn = document.createElement("td");
+  sourceColumn.textContent = elem.source;
+
+  var amountColumn = document.createElement("td");
+  amountColumn.textContent = elem.amount;
+
+  row.append(idColumn, typeColumn, sourceColumn, amountColumn);
+
+  return row;
 };
